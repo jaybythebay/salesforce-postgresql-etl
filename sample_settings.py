028aeae70c937e2a89a7d993ae9793497fac267b
Jay Rosenthal <jay.rosenthal@hover.to>
2018-04-03 08:32:00 -0700

    Fixed blacklist and whitelist samples to be lists instead of tuples

--- sample_settings.py
+++ sample_settings.py
@@ -18,10 +18,10 @@ salesforce_api = {'username': 'USERNAME',
 log_file_path = '/Users/jayrosenthal/Desktop/salesforce_etl.log'
 
 # Object black list.  These are objects to never include even if dumping all from Salesforce otherwise.  The black list overrides the whitelist.
-object_blacklist = ('collaborationgrouprecord', 'contentdocumentlink', 'ideacomment', 'scontrol', 'vote')
+object_blacklist = ['collaborationgrouprecord', 'contentdocumentlink', 'ideacomment', 'scontrol', 'vote']
 
 # Object white list.  This are objects to use.  If this list contains values then salesforce will only get these objects.
-object_whitelist = ('opportunity', 'user', 'account')
+object_whitelist = ['opportunity', 'user', 'account']
 
 # Table prefix.  You must add a prefix to keep tables such as 'user' and 'case' to keep from interferring with PostgreSQL reserved words.
 prefix = "salesforce_"
