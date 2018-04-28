3afa9a54ad736d91946ae83d55987615aa5a74cf
Jay Rosenthal <jay.rosenthal@hover.to>
2018-04-28 11:42:13 -0700

    Reordered import statements

--- salesforce-postgresql.py
+++ salesforce-postgresql.py
@@ -3,11 +3,11 @@ import pandas as pd
 import logging
 from migrate.changeset.schema import rename_table
 
+from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.engine import reflection, create_engine
 from sqlalchemy.engine.url import URL
 from sqlalchemy.schema import MetaData, Table, Column
 from sqlalchemy.inspection import inspect
-from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql.expression import select, text
 from sqlalchemy.sql.functions import func
